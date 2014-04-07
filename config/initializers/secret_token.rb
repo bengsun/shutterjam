79a2c8f9d9abeedcd1282030e7ea64ab121b4506
bengsun <bengsun@gmail.com>
2014-04-07 16:54:27 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shutterjam::Application.config.secret_key_base = 'dbdd4a47b71fe856417c15b4eabbe00e2e34549659ff49ba5989649ecb8522355cef9bf860b0760aa24045a7d77872acec13531f61ee6c06af64072d061bbb44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shutterjam::Application.config.session_store :cookie_store, key: '_shutterjam_session'
